bfae353c4efece684d66d1dbe24c70d495c2e65f
Li Yanzi <yanzi.li@rea-group.com>
2017-02-24 03:11:48 +1100

    implement two view priority width using NSLayoutConstraint

--- Autolayout/BViewController.swift
+++ Autolayout/BViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class BViewController: UIViewController {
   
   let padding: CGFloat = 20.0
+  let redViewMinWidth: CGFloat = 150.0
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -33,13 +34,11 @@ class BViewController: UIViewController {
     redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding).isActive = true
     redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding).isActive = true
     redView.trailingAnchor.constraint(equalTo: greenView.leadingAnchor, constant: -padding).isActive = true
-    let redViewAspectWidth = redView.widthAnchor.constraint(equalTo: greenView.widthAnchor, multiplier:1/3)
-    redViewAspectWidth.priority = UILayoutPriorityDefaultHigh
-    redViewAspectWidth.isActive = true
-    let redViewMinWidth = redView.widthAnchor.constraint(greaterThanOrEqualToConstant: 150)
-    redViewMinWidth.priority = UILayoutPriorityRequired
-    redViewMinWidth.isActive = true
-    
+    let redViewAspectWidthConstraint = redView.widthAnchor.constraint(equalTo: greenView.widthAnchor, multiplier:1/3)
+    redViewAspectWidthConstraint.priority = UILayoutPriorityDefaultHigh
+    redViewAspectWidthConstraint.isActive = true
+    redView.widthAnchor.constraint(greaterThanOrEqualToConstant: redViewMinWidth).isActive = true
+  
     greenView.topAnchor.constraint(equalTo: redView.topAnchor).isActive = true
     greenView.bottomAnchor.constraint(equalTo: redView.bottomAnchor).isActive = true
     greenView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding).isActive = true
@@ -59,7 +58,11 @@ class BViewController: UIViewController {
     NSLayoutConstraint.init(item: redView, attribute: .top, relatedBy: .equal, toItem: topLayoutGuide, attribute: .bottom, multiplier: 1, constant: padding).isActive = true
     NSLayoutConstraint.init(item: redView, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: padding).isActive = true
     NSLayoutConstraint.init(item: redView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: -padding).isActive = true
-    NSLayoutConstraint.init(item: redView, attribute: .width, relatedBy: .equal, toItem: greenView, attribute: .width, multiplier: 1/3, constant: 0).isActive = true
+    let redViewAspectWidthConstraint = NSLayoutConstraint.init(item: redView, attribute: .width, relatedBy: .equal, toItem: greenView, attribute: .width, multiplier: 1/3, constant: 0)
+    redViewAspectWidthConstraint.priority = UILayoutPriorityDefaultHigh
+    redViewAspectWidthConstraint.isActive = true
+    
+    NSLayoutConstraint.init(item: redView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: redViewMinWidth).isActive = true
     NSLayoutConstraint.init(item: redView, attribute: .trailing, relatedBy: .equal, toItem: greenView, attribute: .leading, multiplier: 1, constant: -padding).isActive = true
     NSLayoutConstraint.init(item: greenView, attribute: .top, relatedBy: .equal, toItem: redView, attribute: .top, multiplier: 1, constant: 0).isActive = true
     NSLayoutConstraint.init(item: greenView, attribute: .bottom, relatedBy: .equal, toItem: redView, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
