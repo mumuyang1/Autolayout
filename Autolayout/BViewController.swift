35cf55b06be81d8289bae4e344a0af5e66f036d7
Li Yanzi <yanzi.li@rea-group.com>
2017-02-24 02:18:09 +1100

    implement two equal view using NSLayoutAnchor

---
+++ Autolayout/BViewController.swift
@@ -0,0 +1,41 @@
+//
+//  BViewController.swift
+//  Autolayout
+//
+//  Created by Yanzi Li on 2/24/17.
+//  Copyright © 2017 Yanzi Li. All rights reserved.
+//
+
+import UIKit
+
+class BViewController: UIViewController {
+  
+  let padding: CGFloat = 20.0
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    addRedAndGreenViewUsingNSLayoutAnchor()
+  }
+  
+  func addRedAndGreenViewUsingNSLayoutAnchor() {
+    let redView = UIView()
+    redView.translatesAutoresizingMaskIntoConstraints = false
+    redView.backgroundColor = UIColor.red
+    view.addSubview(redView)
+    
+    let greenView = UIView()
+    greenView.backgroundColor = UIColor.green
+    greenView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(greenView)
+    
+    redView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor, constant: padding).isActive = true
+    redView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding).isActive = true
+    redView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding).isActive = true
+    redView.trailingAnchor.constraint(equalTo: greenView.leadingAnchor, constant: -padding).isActive = true
+    redView.widthAnchor.constraint(equalTo: greenView.widthAnchor).isActive = true
+    
+    greenView.topAnchor.constraint(equalTo: redView.topAnchor).isActive = true
+    greenView.bottomAnchor.constraint(equalTo: redView.bottomAnchor).isActive = true
+    greenView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding).isActive = true
+  }
+}
